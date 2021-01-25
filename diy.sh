cbb4a84a3935c6961b36bb0777e4f6f22449bce9
github112013 <44917863+github112013@users.noreply.github.com>
2021-01-25 13:10:07 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,9 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#
+#添加SSR PLUS+
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
